db6b6c9dad61ec49d30d46422f939b8aff19d5cc
RicolaSalsa <58706@ldcsb.org>
2026-06-03 09:28:07 -0400

    hello

--- test/Icy Tundra/ArenaScript.gd
+++ test/Icy Tundra/ArenaScript.gd
@@ -5,38 +5,53 @@ extends Node2D
 @onready var Collision = $Area2D/CollisionShape2D
 @onready var Camera = $Camera2D
 
+var colour
+
 var max_zoom = 0.62
 
 #x Scale = 57.6
 #y Scale = 32.4
 
-var spawns = [$Player1Spawn, $Player2Spawn, $Player3Spawn, $Player4Spawn]
+@onready var spawns = [$Player1Spawn, $Player2Spawn, $Player3Spawn, $Player4Spawn]
 var scene = preload("res://Players//Player.tscn")
 @export var Player : PackedScene = scene
 
 @onready var ui_scene = preload("res://Health Overlay/RainbowCard.tscn")
+const Laser_ball = preload("res://Ability/Char1/LaserBall/LaserBall.tscn")
 
-func show_ui():
-	
-	var ui_ins = ui_scene.instantiate()
-	
-	$CanvasLayer.add_child(ui_ins)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	show_ui()
-	
-	#Collision.scale.x = 57.6
-	#Collision.scale.y = 32.4
+
+
 	
 	for i in range(len(Globals.PlayerList)):
+	
 		var player_Instance = Player.instantiate()
 		player_Instance.set("PlayerNum", i)
+		player_Instance.set("control", Globals.PlayerList[i])
 		player_Instance.set("position", spawns[i].position)
 		player_Instance.set("Class", Globals.PlayerChars[i])
-		$CanvasLayer.add_child(player_Instance)
-
+		
+		if i == 0:
+			colour = Color(1.0, 0.0, 0.0, 1.0)
+		elif i == 1:
+			colour = Color(1.0, 1.0, 0.0, 1.0)
+		elif i == 2:
+			colour = Color(0.0, 0.0, 1.0, 1.0)
+		elif i == 3:
+			colour = Color(0.0, 1.0, 0.0, 1.0)
+		
+		player_Instance.set("modulate",colour) 
+		#control = Globals.PlayerList[PlayerNum]   
+		var ui_ins = ui_scene.instantiate()
+		
+		player_Instance.health_changed.connect(ui_ins.update_health_bar)
+		add_child(player_Instance)
+		$CanvasLayer/HBoxContainer.add_child(ui_ins)
+		player_Instance.health_changed.emit(player_Instance.current_knock, player_Instance.max_health)
 	
 
 
@@ -50,18 +65,29 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	
 	print("zoom")
-	if $CharacterBody2D.is_in_group("Player") and Camera.zoom >= Vector2(0.6,0.6):
+	if body.is_in_group("Player") and Camera.zoom >= Vector2(0.8,0.8):
 		Camera.zoom -= Vector2(0.2 ,0.2)
 
 func _on_death_floor_body_entered(body: Node2D) -> void:
 	
-	if $CharacterBody2D.is_in_group("Player") and Camera.zoom < Vector2(1,1):
-		Camera.zoom += Vector2(0.2,0.2)
-		
-	#if $CharacterBody2D.is_in_group("Player"):
-		#$CharacterBody2D.queue_free()
+	body.queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	
-		if $CharacterBody2D.is_in_group("Player") and Camera.zoom < Vector2(1,1):
-			Camera.zoom += Vector2(0.2,0.2)
+	if body.is_in_group("Player") and Camera.zoom < Vector2(1,1):
+		Camera.zoom += Vector2(0.2,0.2)
+
+func _on_second_s_tage_body_exited(body: Node2D) -> void:
+	
+	if body.is_in_group("Player") and Camera.zoom >= Vector2(0.6,0.6):
+		Camera.zoom -= Vector2(0.2 ,0.2)
+
+func _on_third_stage_body_exited(body: Node2D) -> void:
+	
+	if body.is_in_group("Player") and Camera.zoom < Vector2(1,1):
+		Camera.zoom += Vector2(0.2,0.2)
+
+func _on_fourth_stage_body_exited(body: Node2D) -> void:
+	
+	if body.is_in_group("Player") and Camera.zoom < Vector2(1,1):
+		Camera.zoom += Vector2(0.2,0.2)
--- test/Players/player.gd
+++ test/Players/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+
+
 @onready var anim = $AnimatedSprite2D
 
 const SPEED = 600.0
@@ -7,10 +9,16 @@ const JUMP_VELOCITY = -600.0
 var jump_num = 0
 var control
 var max_health = 100
-var current_health = 100
-var health_regen = 1
+var current_knock = 1
 var respawn_count = 0
 var anim_over = false
+var move_over = false
+var knock_bonus = 0
+var knock_back = 0
+
+const LASER_BALL_SCENE = preload("res://Ability/Char1/LaserBall/LaserBall.tscn")
+
+signal health_changed(current_knock,max_health)
 
 
 func _ready() -> void:
@@ -22,7 +30,6 @@ func _ready() -> void:
 		control = "C3"
 	elif control == "Controller4":
 		control = "C4"
-
 
 func _physics_process(delta: float) -> void:
 	
@@ -59,22 +66,17 @@ func _physics_process(delta: float) -> void:
 			velocity.y = JUMP_VELOCITY - 100
 			jump_num += 1
 		
-	if Input.is_action_just_pressed("RMB") and not anim_over and control == "Keyboard" or Input.is_action_just_pressed("Controller1Trigger") and not anim_over and control == "C1" or Input.is_action_just_pressed("Controller2Trigger") and not anim_over and control == "C2" or Input.is_action_just_pressed("Controller3Trigger") and not anim_over and control == "C3" or Input.is_action_just_pressed("Controller4Trigger") and not anim_over and control == "C4":
+	if Input.is_action_just_pressed("LMB") and not anim_over and control == "Keyboard" or Input.is_action_just_pressed("Controller1Trigger") and not anim_over and control == "C1" or Input.is_action_just_pressed("Controller2Trigger") and not anim_over and control == "C2" or Input.is_action_just_pressed("Controller3Trigger") and not anim_over and control == "C3" or Input.is_action_just_pressed("Controller4Trigger") and not anim_over and control == "C4":
 		anim_over = true
 		anim.play("punch")
+		$PunchHit/PunchHit/AnimationPlayer.play("punch_hitbox")
 		
 		await anim.animation_finished
 		anim_over = false
-		
-		get_downed_animation()
-		
-	if current_health == 0:
-		
-		death_animation()
-		
-		
+	
+	if Input.is_action_just_pressed("E") and not anim_over and control == "Keyboard" or Input.is_action_just_pressed("Square") and not anim_over and control == "Controller":
+		shoot_laser()
 
-		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction
@@ -90,22 +92,23 @@ func _physics_process(delta: float) -> void:
 		direction = Input.get_axis("Controller4Left", "Controller4Right")
 	if direction and not anim_over:
 		velocity.x = direction * SPEED
-
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	elif move_over == false:
+		velocity.x = move_toward(velocity.x, 0, 100)
 	
 
 
 
 	move_and_slide()
 
-func take_damage(amount: int):
-	current_health -= amount
+func knock_mult(amount: int):
+	current_knock += amount
 	
-	if current_health < 0:
-		current_health = 0
+	if current_knock > 100:
+		current_knock = 100
 		
-	Globals.health_changed.emit(current_health)
+	health_changed.emit(current_knock,100)
+	
+	return (current_knock + amount)
 	
 func get_downed_animation():
 	
@@ -123,4 +126,52 @@ func death_animation():
 	await anim.animation_finished
 	anim_over = false
 
-#func 
+func _on_punch_hit_area_entered(area: Area2D) -> void:
+	
+	if area.name == "Hurtbox" and area.owner != self:
+		
+		if area.owner.has_method("knock_mult"):
+			area.owner.knock_mult(10)
+			
+			
+			area.owner.move_over = true
+			var knock_direction = sign(area.owner.global_position.x - global_position.x)
+			
+
+			if knock_direction == 0: knock_direction = 1 
+			
+			knock_back = 300 * knock_direction
+			knock_bonus = 60 * (area.owner.current_knock/10) * knock_direction
+			
+			print(knock_back)
+			print(area.owner.current_knock)
+			print(knock_bonus)
+			area.owner.velocity.x = knock_back + knock_bonus
+			area.owner.velocity.y = -600 * (0.01*knock_bonus)
+			
+			await get_tree().create_timer(0.5).timeout
+			if area.owner.is_on_floor():
+				area.owner.velocity.x = 0
+				area.owner.velocity.y = 0
+			await get_tree().create_timer(1).timeout
+			area.owner.move_over = false
+			
+func shoot_laser():
+	
+	var laser = LASER_BALL_SCENE.instantiate()
+	
+	if anim.flip_h == true:
+		laser.direction = -1
+		
+		
+	else:
+		laser.direction = 1
+		
+		
+	laser.launcher = self
+	
+	laser.global_position = global_position
+	
+	laser.multiplier = current_knock
+	
+	get_tree().current_scene.add_child(laser)
