22a61baf95c7fed2d80f87e3fbf9c0d460b60f68
RicolaSalsa <58706@ldcsb.org>
2026-06-09 09:31:36 -0400

    hutffgoyguip

--- test/score_screen.gd
+++ test/score_screen.gd
@@ -6,20 +6,22 @@ var Points = [$"1st Spawn", $"2nd Spawn", $"3rd Spawn"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	#if len(Globals.respawns) < 3:
-		#for i in range(len(Globals.respawns)):
-			#var scene = preload("res://Players//Player.tscn")
-			#scene.set("modulate", Colours[i])
-			#scene.set("position", Points[i].position)
-			#scene.set("anim", "defult")
-	#else:
-		#for i in range(3):
-			#var scene = preload("res://Players//Player.tscn")
-			#scene.set("modulate", Colours[i])
-			#scene.set("position", Points[i].position)
-			#scene.set("anim", "defult")
-
+	
+	if len(Globals.respawn_list) < 3:
+		for i in range(len(Globals.respawn_list)):
+			var scene = preload("res://Players//Player.tscn")
+			scene.set("modulate", Colours[Globals.respawn_list[-i]])
+			scene.set("position.x", Points[i].position.x)
+			scene.set("position.y", Points[i].position.y)
+			scene.set("anim", "defult")
+			scene.instantiate()
+	else:
+		for i in range(3):
+			var scene = preload("res://Players//Player.tscn")
+			scene.set("modulate", Colours[Globals.respawn_list[-i]])
+			scene.set("position", Points[i].position)
+			scene.set("anim", "defult")
+			scene.instantiate()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
